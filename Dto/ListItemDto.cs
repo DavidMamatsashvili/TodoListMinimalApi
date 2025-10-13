e814218e56140d0d991179d4892ffdc8c32cd4d8
DavidMamastashvili <mamatsashvilidavid426@gmail.com>
2025-10-14 01:28:49 +0400

    minor changes

--- Dto/ListItemDto.cs
+++ Dto/ListItemDto.cs
@@ -2,10 +2,10 @@ namespace TodoListMinimalApi.Dto
 {
     public class ListItemDto
     {
-        public string Title { get; set; } = null!;
-        public string Content { get; set; } = null!;
+        public string Title { get; set; }
+        public string Content { get; set; }
         public DateTime Date { get; set; }
-        public string Author { get; set; } = null!;
+        public string Author { get; set; } 
     }
 }
 
